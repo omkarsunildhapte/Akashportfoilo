bada3f9ec955d07b04eec668fa0c187ba295b28b
omkarsunildhapte <omkardhapte99@gmail.com>
2023-10-27 20:41:37 +0530

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './compenet/header/header.component';
 import { HomeComponent } from './compenet/home/home.component';
 import { MenuComponent } from './compenet/menu/menu.component';
+import { FooterComponent } from './compenet/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
